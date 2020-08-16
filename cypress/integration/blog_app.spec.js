6f6b97fb1953769c531aced14927d6534a98c05a
papasavvasn <papasavvasn@media-saturn.com>
2020-08-16 23:52:48 +0200

    add login test, handle cy. linting error

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,11 +1,32 @@
+const testUser = {
+    name: 'testName',
+    username: 'testUsername',
+    password: 'testPassword',
+}
+
 describe('Blog app', function () {
     beforeEach(function () {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.request('POST', 'http://localhost:3003/api/users', testUser)
         cy.visit('http://localhost:3000')
     })
 
-    it('Login form is shown', function () {
-        // ...
+    // it('Login form is shown', function () {
+    //     cy.contains('Log in to application')
+    //     cy.contains('Username')
+    //     cy.contains('Password')
+    // })
+    describe('Login', function () {
+        it('succeeds with correct credentials', function () {
+            cy.get('input[name="username"]').type(testUser.username)
+            cy.get('input[name="password"]').type(testUser.password)
+            cy.contains('Login').click()
 
+            cy.contains(`${testUser.name} is logged in`).click()
+        })
+
+        it('fails with wrong credentials', function () {
+            // ..
+        })
     })
 })
