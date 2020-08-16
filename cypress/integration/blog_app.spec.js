f1b77c1ecdd02f6ffe39ef081798298018bb3978
papasavvasn <papasavvasn@media-saturn.com>
2020-08-17 00:07:08 +0200

    complete login test ex.5.18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -11,22 +11,27 @@ describe('Blog app', function () {
         cy.visit('http://localhost:3000')
     })
 
-    // it('Login form is shown', function () {
-    //     cy.contains('Log in to application')
-    //     cy.contains('Username')
-    //     cy.contains('Password')
-    // })
+    it('Login form is shown', function () {
+        cy.contains('Log in to application')
+        cy.contains('Username')
+        cy.contains('Password')
+    })
     describe('Login', function () {
         it('succeeds with correct credentials', function () {
             cy.get('input[name="username"]').type(testUser.username)
             cy.get('input[name="password"]').type(testUser.password)
             cy.contains('Login').click()
 
-            cy.contains(`${testUser.name} is logged in`).click()
+            cy.contains(`${testUser.name} is logged in`)
         })
 
         it('fails with wrong credentials', function () {
-            // ..
+            cy.get('input[name="username"]').type("randomUser")
+            cy.get('input[name="password"]').type("random password")
+            cy.contains('Login').click()
+
+            cy.contains('invalid username or password')
+            cy.contains('invalid username or password').should('have.css', 'color', 'rgb(255, 0, 0)')
         })
     })
 })
